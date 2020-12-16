236ac9f733ee0cdb508284015f6437e0f10ea379
GeniusTimo <mail@geniustimo.de>
2020-12-16 19:22:42 +0100

    Kleine Restrukturierung

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,92 @@
-// Kanäle, die auf doppelte Nachrichten geprüft werden sollen (bitte als Strings angeben!)
-const channelIds: string[] = [];
-// Zeitspanne, in welcher eine Nachricht mit gleichem Inhalt erkannt wird [in Minuten]
-const duplicateTime = 1;
-// Nachricht des Bots bei doppelten Nachrichten (Die Nachricht des Bots wird nach 5s gelöscht, die doppelte sofort)
-const botMessage = 'Bitte keine doppelten Nachrichten senden' + ' ';
-// Soll der Nutzer in der botMessage erwähnt werden?
-const mentionUser = true;
+// Umgebungsvaribalen laden
+import * as dotenv from "dotenv";
+dotenv.config();
 
-// --- AB HIER NICHTS ÄNDERN ---
+// Bibliotheken einbinden
+import cron from 'node-cron';
 import * as Discord from 'discord.js';
-const client = new Discord.Client();
-const channels: (Discord.Channel | undefined)[] = [];
-const messageCache = new Map<Discord.User, Map<string, Date>>();
+const client = new Discord.Client({retryLimit: 5});
+
+// globale Variablen definieren
+const channelIds: string[] = [],
+startTimestamp = new Date().getTime();
+let messageCache = new Map<Discord.Snowflake, Map<string, number>>(),
+warnMessage: string = 'Bitte sende keine doppelten Nachrichten.',
+cooldown: number = 1 * 60 * 1000,
+newMessages: boolean = false,
+mentionUser: boolean = true;
+
+const updateCache = () => {
+	const timestamp = new Date().getTime();
+	if (!newMessages) return; // → keine neuen Nachrichten
+	for (const [user, messages] of messageCache.entries()) {
+		for (const message of messages.entries()) {
+			if (timestamp - message[1] > cooldown) {
+				messages.delete(message[0]);
+			}
+		}
+		messageCache.set(user, messages);
+	}
+};
 
 client.on('ready', async () => {
-	console.log('Bot ready at ' + new Date());
-	for (const channelId of channelIds) {
-		const channel = client.channels.cache.get(channelId);
-		channels.push(channel);
+	console.log('Der Bot ist bereit (' + (new Date().getTime() - startTimestamp) + ' ms)');
+	if (process.env.CHANNELS) {
+		process.env.CHANNELS.split(' ').forEach(channelId => {
+			let channel = client.channels.cache.get(channelId);
+			if (channel?.isText()) channelIds.push(channelId);
+		});
 	}
+	if (process.env.MESSAGE) {
+		warnMessage = process.env.MESSAGE;
+	}
+	if (process.env.MENTION) {
+		mentionUser = Boolean(process.env.MENTION);
+	}
+	if (process.env.TIMER) {
+		cooldown = Number(process.env.TIMER) * 60 * 1000;
+	}
+	console.log('→ Es werden ' + channelIds.length + ' Kanäle beobachtet.');
+	cron.schedule('* * * * *', updateCache);
 });
 
 client.on('message', async (message: Discord.Message) => {
-	for (const [user, userMessages] of messageCache.entries()) {
-		for (const cachedMessage of userMessages.entries()) {
-			const diff = new Date().getTime() - cachedMessage[1].getTime();
-			if (diff > duplicateTime * 60 * 1000) {
-				userMessages.delete(cachedMessage[0]);
-			}
-		}
-		messageCache.set(user, userMessages);
-	}
+	if (message.author.bot || !channelIds.includes(message.channel.id)) return;
+	newMessages = true; // → updateCache() räumt wieder etwas auf
 
-	if(message.author.bot) {return;}
-	if(!channels.includes(message.channel)) {return;}
-
-	if(!messageCache.has(message.author)) {
-		messageCache.set(message.author, new Map<string, Date>([[message.content, new Date()]]));
+	if (!messageCache.has(message.author.id)) {
+		const messages = new Map<string, number>([[message.content, message.createdAt.getTime()]]);
+		messageCache.set(message.author.id, messages);
 		return;
 	}
 
-	const messages: Map<string, Date> = messageCache.get(message.author) as Map<string, Date>;
-	if (messages.get(message.content)) {
-		if (mentionUser) {
-			message.channel.send(botMessage + '<@' + message.author + '>').then(msg => {
-				msg.delete({ timeout: 5000 });
+	const messages = messageCache.get(message.author.id),
+	cachedMessage = messages?.get(message.content);
+	if (cachedMessage) {
+		if (message.createdAt.getTime() - cachedMessage > cooldown) {
+			let embed = new Discord.MessageEmbed().setColor('#F36D6B');
+			embed.setDescription(warnMessage);
+			message.channel.send((mentionUser ? '<@' +message.author.id + '> ⚠' : undefined), embed).then(newMessage => {
+				newMessage.delete({timeout: 5000});
 			});
-		} else {
-			message.channel.send(botMessage).then(msg => {
-				msg.delete({ timeout: 5000 });
-			});
+			message.delete({reason: 'Doppelte Nachricht'});
+			return;
 		}
-		message.delete({ reason: 'Doppelte Nachricht' });
-	} else {
-		messages.set(message.content, new Date());
-		messageCache.set(message.author, messages);
+	}
+	if (messages) {
+		messages.set(message.content, message.createdAt.getTime());
+		messageCache.set(message.author.id, messages);
 	}
 });
 
+process.on('SIGTERM', () => {
+    client.destroy();
+    process.exit(0);
+});
 
-client.login('');
+process.on('SIGINT', () => {
+    client.destroy();
+    process.exit(0);
+});
+
+client.login(process.env.TOKEN);
